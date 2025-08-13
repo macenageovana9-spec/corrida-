bd5b3df1544247b3cac17cf3cf9abfaedcb43dc4
Geovana Macena <macenageovana9@gmail.com>
2025-08-13 14:08:50 -0300

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+ int evento = 5;
+  char corrida = 'c';
+  float tempo = 27.25;
+  
+ printf("o melhor tempo da eliminatória %c", corrida);
+ printf("\n do evento %d foi % f",evento,tempo);
+}
+
